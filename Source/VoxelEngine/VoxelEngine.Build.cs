01dee9c1009ec7f6bfd251271462d0be85dcdf7e
Joey <hernandezj478@yahoo.com>
2026-04-20 23:54:36 -0700

    Renamed Voxel module
    Renamed voxel module to voxel engine

---
+++ Source/VoxelEngine/VoxelEngine.Build.cs
@@ -0,0 +1,25 @@
+using UnrealBuildTool;
+
+public class VoxelEngine: ModuleRules
+{
+    public VoxelEngine(ReadOnlyTargetRules Target) : base(Target)
+    {
+        PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
+
+        PublicDependencyModuleNames.AddRange(
+            new string[]
+            {
+                "Core"
+            }
+        );
+
+        PrivateDependencyModuleNames.AddRange(
+            new string[] 
+            {
+                "CoreUObject", 
+                "Engine",
+                "CoreData"
+            }
+        );
+    }
+}
